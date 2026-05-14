6c46ad074440ef3893943120d9cde851bb972dd2
Bam_Bam <skuy369@gmail.com>
2026-05-15 01:01:15 +0900

    refactor: 버튼 컴포넌트에 추가 className 설정 오류 해결

--- src/components/survey_button.jsx
+++ src/components/survey_button.jsx
@@ -4,18 +4,24 @@ function SurveyButton({
     type,                       // 버튼의 스타일 
     size,                       // 버튼의 크기
     content,                    // 버튼의 내용
+    className = '',             // 추가적인 클래스 이름 (선택 사항)
     isSelected = false,         // 버튼이 선택되었는지 여부
-    onClick = () => {},         // 버튼 클릭 시 실행할 함수 (스타일 변경 및 상태 변경)
+    isActive = false,           // 버튼이 활성화되었는지 여부
+    onClick = () => {},         // 버튼 클릭 시 실행할 함수 (상태 변경)
     ...props                    // 나머지 props 
 }) {
+    const classes = [
+        style.button,                             
+        style[`button-${type}`],                  
+        style[`button-${size}`],                  
+        isSelected ? style.clicked : '' , 
+        isActive ? style.active : '',
+        className,
+    ].filter(Boolean).join(' ')
+
     return (
         <button 
-            className={[
-                style.button,                             
-                style[`button-${type}`],                  
-                style[`button-${size}`],                  
-                isSelected ? style.clicked : ''           
-            ].filter(Boolean).join(' ')}
+            className={classes}
             onClick={onClick}
             {...props}
         >
