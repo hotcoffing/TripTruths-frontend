6de456b311a4a7d9a8760058857764c037b326c3
kmj0973 <kmj0973@naver.com>
2026-05-15 16:52:44 +0900

    feat: 분석 페이지 route 추가

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from 'react-router-dom';
 import RootLayout from '@/layouts/RootLayout';
 import MainPage from '@/pages/MainPage';
+import AnalysisPage from '@/pages/analysisPage/AnalysisPage';
 
 const router = createBrowserRouter([
   {
@@ -9,6 +10,10 @@ const router = createBrowserRouter([
       {
         path: '/',
         element: <MainPage />,
+      },
+      {
+        path: '/analysis',
+        element: <AnalysisPage />,
       },
     ],
   },
