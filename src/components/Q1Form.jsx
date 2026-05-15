4ff5f4802ee93d58bf4235b527455529c30b4186
Bam_Bam <skuy369@gmail.com>
2026-05-16 03:03:47 +0900

    feat: survey Q1 폼 컴포넌트 생성

---
+++ src/components/Q1Form.jsx
@@ -0,0 +1,46 @@
+import style from './Q1Form.module.scss';
+import SurveyInfo from './SurveyInfo';
+import Button from './SurveyButton';
+
+function Q1Form({ 
+        isError,            // Info의 이미지 에러 출력을 위한 변수 
+        isToNext,           // 다음 버튼 활성화를 위한 변수
+        selectList,         // 전체의 선택 리스트
+        nowSelectedList,    // 현재 선택된 선택 리스트
+        handleSelect,       // 선택시 실행되는 핸들러 (선택처리)
+        handleIsNext,       // 다음 버튼을 누르면 전송하는 핸들러 (API 데이터 저장)
+    }) {
+    const countMax = 2;
+
+    return (
+        <div className={style.Q1Container}>
+            <SurveyInfo 
+                type="Q1"
+                isError={isError}
+            />
+            <br />
+
+            <div className={style.selectButtonContainer}>
+                {selectList.map((item) => (
+                    <Button 
+                        key={item.id} 
+                        type="select" 
+                        size="lg" 
+                        content={item.content}
+                        isSelected={nowSelectedList.some((selected) => selected.id === item.id)}
+                        onClick={() => handleSelect(item, nowSelectedList, countMax)}
+                    />
+                ))}
+            </div>
+            <Button
+                type="next-action"
+                size="md"
+                content="다음"
+                isActive={isToNext}
+                onClick={() => handleIsNext("Q2", nowSelectedList)}
+            />
+        </div>
+    )
+}
+
+export default Q1Form;
