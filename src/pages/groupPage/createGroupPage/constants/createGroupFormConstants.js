362f7e38f1949b2d7ad29d32ec9cbfbe2b52bf14
kmj0973 <kmj0973@naver.com>
2026-05-24 00:28:08 +0900

    fix: 그룹 기간 상수 변경

--- src/pages/groupPage/createGroupPage/constants/createGroupFormConstants.js
+++ src/pages/groupPage/createGroupPage/constants/createGroupFormConstants.js
@@ -3,14 +3,14 @@ export const DEFAULT_TRIP_PERIOD = '1박 2일';
 
 export const TRIP_LENGTH_BY_PERIOD = {
   당일치기: 'DAY_TRIP',
-  '1박 2일': 'ONE_NIGHT_TWO_DAYS',
-  '2박 3일': 'TWO_NIGHTS_THREE_DAYS',
-  '3박 4일': 'THREE_NIGHTS_FOUR_DAYS',
+  '1박 2일': 'ONE_NIGHT',
+  '2박 3일': 'TWO_NIGHTS',
+  '3박 4일': 'THREE_NIGHTS_PLUS',
 };
 
 export const PERIOD_BY_TRIP_LENGTH = {
   DAY_TRIP: '당일치기',
-  ONE_NIGHT_TWO_DAYS: '1박 2일',
-  TWO_NIGHTS_THREE_DAYS: '2박 3일',
-  THREE_NIGHTS_FOUR_DAYS: '3박 4일',
+  ONE_NIGHT: '1박 2일',
+  TWO_NIGHTS: '2박 3일',
+  THREE_NIGHTS_PLUS: '3박 4일',
 };
