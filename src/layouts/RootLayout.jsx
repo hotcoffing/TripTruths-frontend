e134d8e0cdd22618b22aad8a3e58e788a37a602e
Bam_Bam <skuy369@gmail.com>
2026-05-28 19:33:01 +0900

    refactor: group 입력 현황 컴포넌트 크기를 키우기 위한 스타일 조정 (pwa, 화면이 큰 스마트폰을 쓸 때의 pc 브라우저 제외)

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -1,8 +1,22 @@
+import { useEffect, useState } from 'react';
 import { Outlet } from 'react-router-dom';
 
 const RootLayout = () => {
+  // PWA 여부 확인
+  const [isPWA, setIsPWA] = useState(false);
+
+  useEffect(() => {
+    const isStandalone =
+      window.matchMedia('(display-mode: standalone)').matches ||
+      window.navigator.standalone;
+    setIsPWA(Boolean(isStandalone));
+  }, []);
+
+  // 목표 : 모바일 브라우저에서만 여백 조정 (PC, pwa 환경에서는 여백 조정 안함)
+  const rootLayoutClassName = isPWA ? 'is-pwa' : 'is-browser';
+
   return (
-    <div>
+    <div className={rootLayoutClassName}>
       <main>
         <Outlet />
       </main>
